92fd641e8f46fb7cb656ea48c188f84420623a5d
Stephen Gelman <ssgelm@gmail.com>
2019-05-23 00:45:21 -0500

    Add Fn Lock functionality with fn+space
    TG(1) would normally do this but I wanted the fn lock to live on the
    fucntion layer (otherwise it's a waste of a key).  The problem with this
    is that calling TG(1) from within layer 1 gets reset when the MO(1) key
    is released.  Therefore, We effectively have to recreate the
    functionality with an exception for the fn lock toggle.

--- keyboards/dz60/keymaps/ssgelm/keymap.c
+++ keyboards/dz60/keymaps/ssgelm/keymap.c
@@ -2,8 +2,12 @@
 
 #define _______ KC_TRNS
 
+bool fnIsLocked = false;
+
 enum custom_keycodes {
-  CLIP = SAFE_RANGE
+  CLIP = SAFE_RANGE,
+  FN_KEY,
+  FN_LOCK,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -25,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
       LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-      MO(1), KC_LALT, KC_LGUI,             LT(1,KC_SPC),                KC_RGUI, KC_RALT, TG(1), KC_RCTL
+      FN_KEY, KC_LALT, KC_LGUI,             LT(1,KC_SPC),                KC_RGUI, KC_RALT, FN_KEY, KC_RCTL
     ),
   /* Keymap 1: Function Layer
    * ,-----------------------------------------------------------.
@@ -45,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
       _______,  KC__MUTE, KC__VOLDOWN, KC__VOLUP, KC_MPLY, KC_PGUP, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_HOME, KC_PGUP, _______,
       _______,   _______, _______, _______,    CLIP, KC_PGDN, _______, _______, KC_DEL, KC_END, KC_PGDN, _______,
-      _______, _______, _______,               _______,                                _______, _______, _______, _______
+      _______, _______, _______,               FN_LOCK,                                _______, _______, _______, _______
     )
 };
 
@@ -54,6 +58,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
       case CLIP:
         SEND_STRING(SS_LCTRL(SS_LALT("v")));
+        return false;
+      case FN_KEY:
+        if (record->event.pressed) {
+          layer_on(1);
+          fnIsLocked = false;
+        } else {
+          if (!fnIsLocked) {
+            layer_off(1);
+          }
+        }
+        return false;
+      case FN_LOCK:
+        if (record->event.pressed) {
+          fnIsLocked = !fnIsLocked;
+
+          if (!fnIsLocked) {
+            layer_off(1);
+          }
+        }
         return false;
     }
   }
